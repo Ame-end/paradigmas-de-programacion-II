0da1a55b09115afff0ed3b656a5f3515d352b5c8
Amelia LapPersonal <ameliaunsij04@gmail.com>
2024-10-15 11:03:31 -0600

    Se crearon los archivos "HelloWorld"

---
+++ Python/Fundamentos-Python/hola_mundo.py
@@ -0,0 +1,9 @@
+# comentarios una linea
+"""
+Amelia Mendoza López
+Descripción: Primer "Hola Mundo" en python
+Fecha:09/10/2024
+"""
+#La impresión puede realizarse con diferentes tipos  de comillas
+print("Hola Mundo") #dobles
+print('Hola Mundo')#simples
