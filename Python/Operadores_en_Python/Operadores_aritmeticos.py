0425c0c80060ddb9c1c9f3826aac473b1cd87898
Amelia LapPersonal <ameliaunsij04@gmail.com>
2024-10-25 10:46:41 -0600

    Actualización de los ejemplos vistos en clase

--- Python/Operadores_en_Python/Operadores_aritmeticos.py
+++ Python/Operadores_en_Python/Operadores_aritmeticos.py
@@ -17,19 +17,22 @@ op_resta = numero_uno - numero_dos  # resta
 op_mult = numero_uno * numero_dos  # multiplicación
 op_div = numero_uno / numero_dos  # división
 op_mod = numero_uno % numero_dos  # módulo
-op_prueb1 = numero_uno ** numero_dos #exponente
+op_prueb1 = numero_uno ** numero_dos  # exponente
 op_prueb2 = numero_uno // numero_dos  # Obtiene el valor entero de la división
 
+print(f"El resultado de la suma [+] entre {numero_uno} y {numero_dos} es: {op_suma}")
+print(f"El resultado de la resta [-] entre {numero_uno} y {numero_dos} es: {op_resta}")
+print(f"El resultado de la división [/] entre {numero_uno} y {numero_dos} es: {op_div:.3f}")
+print(f"El resultado de la multiplicación [*] entre {numero_uno} y {numero_dos} es: {op_mult}")
+print(f"El resultado del modúlo entre [%] {numero_uno} y {numero_dos} es: {op_mod}")
+print(f"La prueba de exponenciación [**] entre {numero_uno} y {numero_dos} es: {op_prueb1}")
+print(f"La prueba de división entera [//] entre {numero_uno} y {numero_dos} es: {op_prueb2}")
+
 '''
+NOTA:
+
 CONCATENACIÓN con "f{" "}":
 La letra "f" hace referencia a una "cadena formateada", esto indica 
 la implementación de llaves dentro de la cadena, donde se 
 almacenan los valores asignados a cada variable.
 '''
-print(f"El resultado de la suma entre {numero_uno} y {numero_dos} es: {op_suma}")
-print(f"El resultado de la resta entre {numero_uno} y {numero_dos} es: {op_resta}")
-print(f"El resultado de la división entre {numero_uno} y {numero_dos} es: {op_div:.3f}")
-print(f"El resultado de la multiplicación entre {numero_uno} y {numero_dos} es: {op_mult}")
-print(f"El resultado del modúlo entre {numero_uno} y {numero_dos} es: {op_mod}")
-print(f"La prueba de ** entre {numero_uno} y {numero_dos} es: {op_prueb1}")
-print(f"La prueba de // entre {numero_uno} y {numero_dos} es: {op_prueb2}")
