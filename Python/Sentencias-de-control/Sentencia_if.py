8a189928db0761e8e69658efc8c32cdf3f81415b
Amelia LapPersonal <ameliaunsij04@gmail.com>
2024-10-28 11:11:03 -0600

    Actualización de los ejemplos vistos en clase

---
+++ Python/Sentencias-de-control/Sentencia_if.py
@@ -0,0 +1,20 @@
+'''
+Nombre: Amelia Mendoza López
+Fecha:23/10/2024
+Descripción: Determina el comportamiento del if y tabuladores en Python
+'''
+
+print("*************MAYORES DE EDAD***********")
+#Se realiza un castinf de variables (cadena-entero)
+#y se almacena en la variable "edad"
+edad = int(input("Ingresa tu edad: "))
+
+#Se establece una condición que se evaluará previamente
+if edad >= 18:
+    print("Tú eres mayor de edad")
+    print("Código de ejemplo")
+
+    '''
+    Todo código que este dentro de la condición
+    deberá respetar la tabulación para ser ejecutado por este
+    '''
