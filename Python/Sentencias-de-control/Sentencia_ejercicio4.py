329eeeea597ad69110c4b581ea9bda863c993bdf
Amelia LapPersonal <ameliaunsij04@gmail.com>
2024-10-29 00:08:56 -0600

    Actualización de los ejemplos vistos en clase

---
+++ Python/Sentencias-de-control/Sentencia_ejercicio4.py
@@ -0,0 +1,21 @@
+'''
+Nombre: Amelia Mendoza López
+Fecha:24/10/2024
+Descripción: Implementación de
+'''
+
+print("************* RESTRICCIÓN DE EDAD***********")
+# Solicitar edad
+#Realiza un casting de varibles
+edad = int(input("Ingresa tu edad: "))
+
+# Solicitar cantidad de dinero
+dinero = float(input("Ingresa el dinero con el que cuentas: "))
+
+# Verificar las condiciones de entrada
+if edad >= 18 and dinero >= 250.0:
+    mensaje = "¡Bienvenido a tu mejor bar!"
+else:
+    mensaje = "Lo sentimos, ya estamos por cerrar!"
+
+print(mensaje)
