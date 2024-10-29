c22369d8f802d4a6c640ba84e0a2e6ac847a7b9f
Amelia LapPersonal <ameliaunsij04@gmail.com>
2024-10-29 06:19:13 -0600

    Actualización de los ejemplos vistos en clase

---
+++ Python/Ciclos/ciclo_ejercicio1.py
@@ -0,0 +1,27 @@
+'''
+Nombre: Amelia Mendoza López
+Fecha:24/10/2024
+Descripción:
+'''
+
+print("********* CICLOS *********")
+print("********  while  *******")
+print("*****  EJERCICIO 1 *******")
+print("suma acumulativa de dos valores")
+
+num = int(input("Digite el número final: "))
+
+# Inicializar la variable en 0
+i = 0                # Contador que comienza desde 0
+acumulador = 0       # Acumulador para sumar los valores de 'i'
+
+# Ciclo while que imprime y suma números hasta el valor de 'num'
+while i <= num:
+    print(i, end=" ")            # Imprime el valor actual de 'i' en la misma línea
+    acumulador = acumulador + i   # Agrega el valor de 'i' al acumulador
+    i = i + 1                     # Incrementa 'i' en 1 para la siguiente iteración
+print()  # Salto de línea final
+print(f"La suma del 0 hasta {num} es: {acumulador}")
+
+
+
