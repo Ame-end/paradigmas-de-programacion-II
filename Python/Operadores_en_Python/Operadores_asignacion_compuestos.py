1e7947fbe49b01408627a83898bbcbc9ef2a9c3f
Amelia LapPersonal <ameliaunsij04@gmail.com>
2024-10-28 23:07:18 -0600

    Actualización de los ejemplos vistos en clase

---
+++ Python/Operadores_en_Python/Operadores_asignacion_compuestos.py
@@ -0,0 +1,43 @@
+
+'''
+Nombre: Amelia Mendoza López
+Fecha: 20/10/2024
+Descripción: Implementación del uso compuesto de los diferentes operadores de asignación.
+'''
+
+
+print("************ OPERADORES DE ASIGNACIÓN ************")
+print("***************** Compuestos *****************")
+
+# Entrada de datos
+num1 = int(input("Ingresa el primer número: "))
+num2 = int(input("Ingresa el segundo número: "))
+
+# Operadores de asignación compuestos
+# Suma en asignación
+num1 += 4
+print("Resultado de num1 += 4:", num1)
+
+# Resta en asignación
+num2 -= 6
+print("Resultado de num2 -= 6:", num2)
+
+# Multiplicación en asignación
+num1 *= 21
+print("Resultado de num1 *= 21:", num1)
+
+# División en asignación
+num2 /= 11
+print("Resultado de num2 /= 11:", num2)
+
+# Módulo en asignación
+num1 %= 14
+print("Resultado de num1 %= 14:", num1)
+
+# Potencia en asignación
+num2 **= 2
+print("Resultado de num2 **= 2:", num2)
+
+# División entera en asignación
+num2 //= 3
+print("Resultado de num2 //= 3:", num2)
