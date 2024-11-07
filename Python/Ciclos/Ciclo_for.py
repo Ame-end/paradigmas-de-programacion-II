5e083072308e54ede7765d08b88333dc5d7f082b
Amelia LapPersonal <ameliaunsij04@gmail.com>
2024-11-07 10:20:14 -0600

    EjercicioPrimero del examen POO 2

---
+++ Python/Ciclos/Ciclo_for.py
@@ -0,0 +1,29 @@
+'''
+Nombre: Amelia Mendoza López
+Fecha:29/10/2024
+Descripción: Fundamentos para el ciclo for
+'''
+
+print("************EJEMPLO DE CICLO FOR****************")
+
+cadena_uno = input("Ingresa una cadena: ")
+for caracter in cadena_uno:
+    print(caracter, end="-")
+
+'''
+print("**************")
+print("*******ALUMNOS DE QUINTO SEMESTRE*******")
+alumnos_quinto = [
+    "Albert",
+    "Kevin",
+    "Elton",
+    "Diana",
+    "Edén",
+    "Amelia",
+    "Sergio"
+    ]
+
+for alumno in alumnos_quinto:
+    print(f"Hola {alumno}")
+
+'''
