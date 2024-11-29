0d3acb56d367c50570f159ed5fc92f592da5f488
Amelia LapPersonal <ameliaunsij04@gmail.com>
2024-11-29 15:54:28 -0600

    EjercicioPrimero del examen POO 2

--- Python/Ciclos/Ciclo_for.py
+++ Python/Ciclos/Ciclo_for.py
@@ -2,15 +2,27 @@
 Nombre: Amelia Mendoza López
 Fecha:29/10/2024
 Descripción: Fundamentos para el ciclo for
+
+          CICLO FOR
+El bucle for se utilizado para recorrer los elementos de un
+objeto iterable (lista, tupla, conjunto, diccionario..) y
+ejecutarun bloque de código. En cada paso de la iteración
+se tiene en cuenta a un único elemento del objeto iterable
+sobre el cuál se puede aplicar una serie de operaciones
+
+Sintaxis
+    for <elem> in <iterable>:
+        <código>
 '''
 
 print("************EJEMPLO DE CICLO FOR****************")
 
 cadena_uno = input("Ingresa una cadena: ")
+# "caracter" itera dentro de cada carácter de "cadena_uno"
 for caracter in cadena_uno:
     print(caracter, end="-")
 
-'''
+
 print("**************")
 print("*******ALUMNOS DE QUINTO SEMESTRE*******")
 alumnos_quinto = [
@@ -22,8 +34,7 @@ alumnos_quinto = [
     "Amelia",
     "Sergio"
     ]
-
+# "alumno" itera dentro de cada carácter de "alumnos_quinto"
 for alumno in alumnos_quinto:
     print(f"Hola {alumno}")
 
-'''
